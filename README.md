8610b0cfc9aa8989e86aa3861496351859ee6475
guanbo <guanbo2002@gmail.com>
2014-02-03 20:46:04 +0800

    Add：服务

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 泛卡汇官方网站
 ================
 
+利用[Node.js](http://nodejs.org)以及[gulp](https://github.com/gulpjs/gulp)和[livereload chrome插件](https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei) 
 
+## QuickStart
+
+
+    cd path/to/fankahui.github.io/
+    npm install
+    gulp
+  
+启动Chrome的[livereload](https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei) 插件
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var styl = require('gulp-styl');
+var refresh = require('gulp-livereload');
+var lr = require('tiny-lr');
+var server = lr();
+
+gulp.task('scripts', function() {
+  gulp.src(['app/js/**/*.js'])
+    .pipe(browserify())
+    .pipe(concat('dest.js'))
+    .pipe(gulp.dest('build'))
+    .pipe(refresh(server))
+})
+
+gulp.task('styles', function() {
+  gulp.src(['app/css/**/*.css'])
+    .pipe(styl({
+      compress: true
+    }))
+    .pipe(gulp.dest('build'))
+    .pipe(refresh(server))
+})
+
+gulp.task('lr-server', function() {
+  server.listen(35729, function(err) {
+    if (err) return console.log(err);
+
+    gulp.watch(['js/**/*.js', '*.html', 'css/**/*.css'], function(event) {
+      gulp.src('index.html').pipe(refresh(server))
+    })
+  });
+})
+
+gulp.task('build', function() {
+  gulp.run('lr-server', 'scripts', 'styles');
+})
+
+gulp.task('default', function() {
+  gulp.run('lr-server');
+})
